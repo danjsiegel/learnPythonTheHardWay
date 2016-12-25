e5b43d258cd1024e4165e62975d9c5e1f4e35c53
Dan <dan.j.siegel@gmail.com>
2016-12-24 18:12:34 -0800

    exercise 7

---
+++ ex7.py
@@ -0,0 +1,22 @@
+print "Mary had a little lamb."
+print "It's fleece was with as %s." % 'snow' #inserts a string snow
+print "and everywher that mary went."
+print "." * 10 # what that do?  #replicates string "." 10 times, or just 10 .'s 
+
+end1 = "C"
+end2 = "h"
+end3 = "e"
+end4 = "e"
+end5 = "s"
+end6 = "e"
+end7 = "B"
+end8 = "u"
+end9 = "r"
+end10 = "g"
+end11 = "e"
+end12 = "r"
+
+#watch that comma at the end. try removing it.
+print end1 + end2 + end3 + end4 + end5 + end6, #comma makes both prints display on same line. no comma makes a carriage return between cheese and burger 
+print end7 + end8 + end9 + end10 + end11 + end12
+
