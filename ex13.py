05e96103a5a934abda430b391f29b288aa96b0df
Dan <dan.j.siegel@gmail.com>
2016-12-25 16:40:58 -0800

    exercise 13 and the study drill

---
+++ ex13.py
@@ -0,0 +1,10 @@
+from sys import argv
+
+script, first, second, third = argv
+
+print "This scfript is called:", script
+print "Your first variable is:", first
+print "your second variable is:", second
+print "your third variable is:", third
+
+
---
+++ ex13studydrill.py
@@ -0,0 +1,11 @@
+from sys import argv
+
+scriptname, var1, var2 = argv
+
+print "this is var1", var1
+print "this is var2", var2
+
+var3 = raw_input("what is your var4?: ") 
+print "this is var4 %s" % var3
+
+
