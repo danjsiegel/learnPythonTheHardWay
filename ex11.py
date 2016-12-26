223f02ec6953421a2077f12ad38fb9fa6ee8c348
Dan <dan.j.siegel@gmail.com>
2016-12-25 16:20:16 -0800

    exercise 11 and practice

---
+++ ex11.py
@@ -0,0 +1,16 @@
+print "How old are you?",
+age = raw_input()
+print "how tall are you?",
+height = raw_input()
+print "how much do you weigh?",
+weight = raw_input()
+
+print "So you're %r old, %r all and %r heavy." % (age, height, weight)
+
+
+name = raw_input("what is your name>> ")
+print "%r" % name
+
+number = int(raw_input( "say 5>>"))
+print "%r" % number
+print number
